0a14d943a0aff8f50ce7264709b0209abdc4a1ee
antoinemourat <antoinemourat@gmail.com>
2019-12-09 11:56:40 +0100

    update: page register

---
+++ register.php
@@ -0,0 +1,26 @@
+<?php require_once 'includes/header.php'; ?>
+
+    <div class="card container">
+            <form>
+
+                <div class="form-group">
+                    <label for="exampleInputPseudo1">Pseudo</label>
+                    <input type="text" class="form-control" id="exampleInputPseudo1">
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            </div>
+
+
+<?php require_once 'includes/footer.php'; ?>
